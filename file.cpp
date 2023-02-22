eb5d9852ce286a8d81e56ba4a11faedcf6e3f450
Hardikmaind <maindhardik@gmail.com>
2023-02-22 14:39:35 +0530

    quicksort funtions added

--- file.cpp
+++ file.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
 using namespace std;
+
+void quicksort(){
+//this is the funtion of the quick sort
+}
 int main(){
 cout<<"hello world"<<endl;
 cout<<"Adi is my best friend"<<endl;
